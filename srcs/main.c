b8df669eff0a07e19449dd9d5f4cd6c2dc0b6ff7
Antonio Panthappallil <apanthap@e4r5p1.42roma.it>
2021-03-11 16:46:58 +0100

    ch2

--- srcs/main.c
+++ srcs/main.c
@@ -88,7 +88,7 @@ void	print_status(t_scene scene)
 	l_elem = scene.light;
 	while (l_elem)
 	{
-		printf(" Light %d _.::._ t: %lf\t rgb: %x\t p.x: %lf\t p.y: %lf\t p.z: %lf\t next: %p\n " \
+		printf("Light %d _.::._ t: %lf\t rgb: %x\t p.x: %lf\t p.y: %lf\t p.z: %lf\t next: %p\n" \
 			, i++, l_elem->t, l_elem->trgb, l_elem->p.x, l_elem->p.y, l_elem->p.z, l_elem->next);
 		l_elem = l_elem->next;
 	}
@@ -96,7 +96,7 @@ void	print_status(t_scene scene)
 	i = 1;
 	while (c_elem)
 	{
-		printf(" Camera %d _.::._ p.x: %lf\t p.y: %lf\t p.z: %lf\t v.x: %lf\t v.y: %lf\t v.z: %lf\t fov: %d\t next: %p\n" \
+		printf("Camera %d _.::._ p.x: %lf\t p.y: %lf\t p.z: %lf\t v.x: %lf\t v.y: %lf\t v.z: %lf\t fov: %d\t next: %p\n" \
 			, i++, c_elem->p0.x, c_elem->p0.y, c_elem->p0.z, c_elem->v.x, c_elem->v.y, c_elem->v.z, c_elem->fov, c_elem->next);
 		c_elem = c_elem->next;
 	}
@@ -104,31 +104,31 @@ void	print_status(t_scene scene)
 	i = 1;
 	while(o_elem)
 	{
-		printf(" Obj %d _.::._ id: %d\n\t\t",  i++, o_elem->id);
+		printf("Obj %d _.::._ id: %d\n\t\t",  i++, o_elem->id);
 		if(o_elem->id == 0)
 		{
-			t_sph sphere = o_elem->obj;
-			printf(" Sphere -_-_- center.x: %lf\tcenter.y: %lf\tcenter.z: %lf", sphere->c->x, sphere->c->y, sphere->c->z);
+			t_sph *sphere = (t_sph*)o_elem->obj;
+			printf(" Sphere -_-_- center.x: %lf\tcenter.y: %lf\tcenter.z: %lf", sphere->c.x, sphere->c.y, sphere->c.z);
 		}
 		if(o_elem->id == 1)
 		{
-			t_plane plane = o_elem->obj;
-			printf(" Plane -_-_- coord.x: %lf\tcoord.y: %lf\tcoord.z: %lf", plane->p0->x, plane->p0->y, plane->p0->z);
+			t_plane *plane = (t_plane*)o_elem->obj;
+			printf(" Plane -_-_- coord.x: %lf\tcoord.y: %lf\tcoord.z: %lf", plane->p0.x, plane->p0.y, plane->p0.z);
 		}
 		if(o_elem->id == 2)
 		{
-			t_square square = o_elem->obj;
-			printf(" Square -_-_- center.x: %lf\tcenter.y: %lf\tcenter.z: %lf", square->c->x, square->c->y, square->c->z);
+			t_square *square = (t_square*)o_elem->obj;
+			printf(" Square -_-_- center.x: %lf\tcenter.y: %lf\tcenter.z: %lf", square->c.x, square->c.y, square->c.z);
 		}
 		if(o_elem->id == 3)
 		{
-			t_cyl cylinder = o_elem->obj;
-			printf(" Cylinder -_-_- coord.x: %lf\tcoord.y: %lf\tcoord.z: %lf", cylinder->c->x, cylinder->c->y, cylinder->c->z);
+			t_cyl *cylinder = (t_cyl*)o_elem->obj;
+			printf(" Cylinder -_-_- coord.x: %lf\tcoord.y: %lf\tcoord.z: %lf", cylinder->c.x, cylinder->c.y, cylinder->c.z);
 		}
 		if(o_elem->id == 4)
 		{
-			t_triangle triangle = o_elem->obj;
-			printf(" Triangle -_-_- point1.x: %lf\tpoint1.y: %lf\tpoint1.z: %lf", triangle->p1->x, triangle->p1->y, triangle->p1->z);
+			t_triangle *triangle = (t_triangle*)o_elem->obj;
+			printf(" Triangle -_-_- point1.x: %lf\tpoint1.y: %lf\tpoint1.z: %lf", triangle->p1.x, triangle->p1.y, triangle->p1.z);
 		}
 		o_elem = o_elem->next;
 	}
